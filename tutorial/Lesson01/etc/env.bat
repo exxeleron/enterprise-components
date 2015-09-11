cfbe6d50cdaf2bd03fa09f2f000a77577ba9d1ca
Joanna Jarmulska <j.wdowiak@devnet.de>
2015-09-11 09:39:04 +0200

    code cleaning in env files

--- tutorial/Lesson01/etc/env.bat
+++ tutorial/Lesson01/etc/env.bat
@@ -1,28 +1,29 @@
 @ECHO OFF
 IF NOT EXIST BIN GOTO NOBIN
 
-set EC_SYS_PATH=%CD%
+SET EC_SYS_PATH=%CD%
 
 REM  ---- q enviroment ----
-IF EXIST %EC_SYS_PATH%\bin\q\w32\q.exe set QHOME=%EC_SYS_PATH%\bin\q
-IF EXIST %EC_SYS_PATH%\bin\q\w64\q.exe set QHOME=%EC_SYS_PATH%\bin\q
-set PATH=%QHOME%\w64;%QHOME%\w32;%PATH%
-q.exe 2> NUL
-IF %ERRORLEVEL%==9009 GOTO NOQ
-REM in case of q64bit - it is recomended to set QLIC as following: QLIC=%EC_SYS_PATH%\etc
-
-REM ---- yak enviroment ----
-set YAK_PATH=%EC_SYS_PATH%\bin\yak
-set YAK_OPTS=-c %EC_ETC_PATH%/system.cfg -s %EC_SYS_PATH%/data/yak/yak.status -l %EC_SYS_PATH%/log/yak/yak.log
-set PATH=%YAK_PATH%;%PATH%
+IF EXIST %EC_SYS_PATH%\bin\q\w32\q.exe SET QHOME=%EC_SYS_PATH%\bin\q
+IF EXIST %EC_SYS_PATH%\bin\q\w64\q.exe SET QHOME=%EC_SYS_PATH%\bin\q
+SET PATH=%QHOME%\w64;%QHOME%\w32;%PATH%
+WHERE /q q.exe || GOTO NOQ
+REM in case of q64bit - it is recommended to SET QLIC as following: QLIC=%EC_SYS_PATH%\etc
 
 REM ---- ec enviroment ----
-set EC_QSL_PATH=%EC_SYS_PATH%/bin/ec/libraries/qsl/
-set EC_ETC_PATH=%EC_SYS_PATH%/etc
+SET EC_QSL_PATH=%EC_SYS_PATH%/bin/ec/libraries/qsl/
+SET EC_ETC_PATH=%EC_SYS_PATH%/etc
 
-set EC_SYS_ID="DemoSystem"
-set EC_SYS_TYPE="Lesson01"
-echo env for system %EC_SYS_ID%/%EC_SYS_TYPE% loaded
+SET EC_SYS_ID="DemoSystem"
+SET EC_SYS_TYPE="Lesson01"
+
+REM ---- yak enviroment ----
+SET YAK_PATH=%EC_SYS_PATH%\bin\yak
+SET YAK_OPTS=-c %EC_ETC_PATH%/system.cfg -s %EC_SYS_PATH%/data/test/%EC_SYS_ID%/yak/yak.status -l %EC_SYS_PATH%/log/test/%EC_SYS_ID%/yak/yak.log
+SET PATH=%YAK_PATH%;%PATH%
+
+ECHO env for system %EC_SYS_ID%/%EC_SYS_TYPE% loaded
+
 
 GOTO END
 :NOBIN
